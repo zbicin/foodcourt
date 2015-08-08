8bcd84c115d00e93db21add045d13ec3a8dfe26a
Krzysztof Zbiciński <fistasheq+github@gmail.com>
2015-08-08 11:06:21 +0200

    asp.net project init

---
+++ FoodCourt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodCourt.Startup))]
+namespace FoodCourt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
