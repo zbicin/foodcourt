b4f7db4ee981675c95e2caa23542b9fc4f9d7cfb
Krystian Sitarek <krystian@virti.eu>
2015-08-09 11:19:01 +0200

    SendGrid reference added, postman draft

---
+++ FoodCourt.Service/Postman.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using SendGrid;
+
+namespace FoodCourt.Service
+{
+    public class Postman
+    {
+        private string _apiKey;
+        private string _sender;
+
+        const string SendgridApiKey = "SendGridApiKey";
+        const string SendgridSender = "SendGridSender";
+
+        public Postman()
+        {
+            _apiKey = ConfigurationManager.AppSettings[SendgridApiKey] as string;
+            _sender = ConfigurationManager.AppSettings[SendgridSender] as string;
+
+            var transport = new Web(_apiKey);
+        }
+
+        public void Send()
+        {
+            
+        }
+    }
+}
