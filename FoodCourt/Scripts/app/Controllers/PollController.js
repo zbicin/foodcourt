006b6eaa57f9f8cd934a8175c00fb0f68e7c7db7
Krzysztof Zbiciński <fistasheq+github@gmail.com>
2015-08-09 10:41:47 +0200

    poll in progress; resolved errors indicated by resharper to calm down Krystian

--- FoodCourt/Scripts/app/Controllers/PollController.js
+++ FoodCourt/Scripts/app/Controllers/PollController.js
@@ -1,4 +1,12 @@
-﻿angular.module('FoodCourtApp').controller('PollController', ['$scope', 'PollService', function ($scope, PollService) {
+﻿angular.module('FoodCourtApp').controller('PollController', ['$scope', 'PollService', 'KindService', function ($scope, PollService, KindService) {
+    $scope.kinds = [];
+
+    $scope.newOrder = {
+        kindId: null,
+    };
+
+    $scope.getRestaurantsForKind = getRestaurantsForKind;
+
     PollService.tryGetCurrentPoll().then(function (poll) {
         console.log(poll);
     }, function (error) {
@@ -6,4 +14,23 @@
         alert('Something went terribly wrong. See console for details.');
     });
 
+    KindService.getList().then(function (response) {
+        $scope.kinds = response.data;
+    }, function (error) {
+        console.log(error);
+    });
+
+    // public ------
+    function getRestaurantsForKind(kind) {
+        return [
+            {
+                Name: 'rest 1',
+                MenuUrl: 'http://google.pl'
+            },
+            {
+                Name: 'rest 1',
+                MenuUrl: 'http://google.pl'
+            }
+        ];
+    }
 }]);
