0984c2c1745ba0b10997d5b706e312d8d3c33a17
Krystian Sitarek <krystian@virti.eu>
2015-08-09 20:30:49 +0200

    Poll finish in progress

--- FoodCourt/Controllers/PollController.cs
+++ FoodCourt/Controllers/PollController.cs
@@ -1,15 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
+using System.Web.Http.Results;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
 using FoodCourt.Controllers.Base;
 using FoodCourt.Lib;
 using FoodCourt.Model;
+using FoodCourt.Service;
 using FoodCourt.ViewModel;
 
 namespace FoodCourt.Controllers
@@ -54,6 +58,70 @@ namespace FoodCourt.Controllers
             }
 
             return Json(currentPollViewModel, JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<ActionResult> FinishPoll()
+        {
+            Poll poll = await UnitOfWork.PollRepository.GetCurrentForGroup(CurrentGroup, "Orders, CreatedBy").SingleAsync();
+
+            if (poll.CreatedBy.Id != CurrentUser.Id)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized, "Unauthorized");
+            }
+
+            List<Order> orders = poll.Orders.ToList();
+            var matches = OrderMatchHandler.ProcessOrders(orders);
+
+            if (poll.IsFinished)
+            {
+                if (poll.IsResolved)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Poll already closed");
+                }
+
+                poll.IsResolved = true;
+                await UnitOfWork.PollRepository.Update(poll);
+
+                // send confirmations
+                SendNotifications(poll, orders);
+                UpdateLastOrderDates(matches);
+            }
+            else
+            {
+                poll.IsFinished = true;
+                poll.IsResolved = matches.Any(m => m.MatchedOrders.Count() == 1) == false;
+
+                await UnitOfWork.PollRepository.Update(poll);
+
+                if (poll.IsResolved)
+                {
+                    SendNotifications(poll, orders);
+                    UpdateLastOrderDates(matches);
+                }
+                else
+                {
+                    var singleOrders = matches.Where(o => o.MatchedOrders.Count() == 1)
+                        .SelectMany(o => o.MatchedOrders)
+                        .Select(o => o.Id).ToList();
+
+                    SendFinalizeWarnings(poll, singleOrders);
+                }
+            }
+
+            return Json("");
+        }
+
+        private void UpdateLastOrderDates(List<OrderBasket> matches)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SendFinalizeWarnings(Poll poll, List<Guid> singleOrders)
+        {
+        }
+
+        private void SendNotifications(Poll poll, List<Order> orders)
+        {
         }
 
         private IQueryable<PollViewModel> GetViewModelQuery(IQueryable<Poll> query)
